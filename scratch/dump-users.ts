b66ad79a9886a1b1de8db24f612fa28782cf94bb
WLD's MacBook <digitalmuazhere@gmail.com>
2026-06-08 22:09:02 +0500

    chore: fix user dump script fields

--- scratch/dump-users.ts
+++ scratch/dump-users.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 async function main() {
   const users = await prisma.user.findMany({
-    select: { email: true, role: true }
+    select: { email: true, name: true }
   });
   console.log(JSON.stringify(users, null, 2));
 }
